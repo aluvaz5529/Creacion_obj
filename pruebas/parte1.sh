a2be9fde774d8791ae42d2ccf75e11d259c621ef
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-02-15 17:08:40 +0100

    Create parte1.sh

---
+++ pruebas/parte1.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt remove --purge samba -y
+sudo apt remove --purge smbclient -y
+sudo apt remove --purge winbind -y
+sudo apt remove --purge krb5-config -y
+sudo apt upgrade
+sudo apt install samba -y
+sudo apt install krb5-config -y
+sudo apt install winbind y-
+sudo apt install smbclient -y
